654cec8d4d986cda55a8c2557fb9675373de1cfe
gooney <gooney.me@outlook.com>
2024-01-08 21:31:22 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,40 @@
-import {nextui} from '@nextui-org/theme'
-import typographyPlugin from '@tailwindcss/typography'
-import { type Config } from 'tailwindcss'
-import typographyStyles from './typography'
-
-
-
+import { nextui } from "@nextui-org/theme";
+import typographyPlugin from "@tailwindcss/typography";
+import { type Config } from "tailwindcss";
+import typographyStyles from "./typography";
 
 export default {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
     typography: typographyStyles,
   },
   darkMode: "class",
-  plugins: [nextui(), typographyPlugin()],
-}  satisfies Config
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          layout: {
+            
+          },
+          colors: {
+            
+          },
+        },
+        dark: {
+          layout: {}, 
+          colors: {
+            background: "rgb(15, 23, 42)",
+            // content1: "rgb(15, 23, 42)",
+          }, 
+        },
+      },
+    }),
+    typographyPlugin(),
+  ],
+} satisfies Config;
